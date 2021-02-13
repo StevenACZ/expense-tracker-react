07fd350e9669d3f4ed1c5c90ce78c2bc2a9016d6
stevenacz <scoailazaa@gmail.com>
2021-02-12 19:51:51 -0500

    query to Balance component added

--- src/components/TransactionItem.js
+++ src/components/TransactionItem.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
 export const TransactionItem = ({ text, amount }) => {
+
+  const sign = amount < 0 ? '-' : '+';
+
   return (
-    <li className="minus">
-      { text } <span>${ amount }</span><button className="delete-btn">x</button>
+    <li className={ amount < 0 ? 'minus' : 'plus' }>
+      { text } <span>{ sign }${ Math.abs(amount) }</span>
+      <button className="delete-btn">x</button>
     </li>
   )
 }
