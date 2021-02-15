b7d0346f5d258a4439a87b7314d9e00e2b5a4a28
stevenacz <scoailazaa@gmail.com>
2021-02-15 16:27:46 -0500

    numberWithCommas added

--- src/components/TransactionItem.js
+++ src/components/TransactionItem.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { GlobalContext } from '../context/GlobalState';
+import { numberWithCommas } from '../utils/format';
 
 export const TransactionItem = ({ _id, text, amount }) => {
 
@@ -9,7 +10,7 @@ export const TransactionItem = ({ _id, text, amount }) => {
   
   return (
     <li className={ amount < 0 ? 'minus' : 'plus' }>
-      { text } <span>{ sign }${ Math.abs( amount ) }</span>
+      { text } <span>{ sign }${ numberWithCommas(Math.abs( amount )) }</span>
       <button
         className="delete-btn"
         onClick={ () => deleteTransaction( _id ) }
